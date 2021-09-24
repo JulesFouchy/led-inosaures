c19bc48806053d0e635689d3f395b756d00b557b
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-24 18:01:17 +0200

    Add fade

--- arduino/led_inosaures/led_inosaures.ino
+++ arduino/led_inosaures/led_inosaures.ino
@@ -20,10 +20,15 @@ uint8_t blue(uint32_t col)
     return col & 255;
 }
 
+int clamp(int x)
+{
+    return min(max(x, 0), 255);
+}
+
 void gradient(Adafruit_CPlay_NeoPixel& strip)
 {
-    static int frame = 0;
     strip.setBrightness(255);
+    static int frame = 0;
     for (int i = 0; i < nb_leds; ++i) {
         float t = sin(frame * 0.01 + i * 0.1) * 0.5 + 0.5;
         int   r = t * 255;
@@ -33,6 +38,30 @@ void gradient(Adafruit_CPlay_NeoPixel& strip)
     frame = (frame + 1);
 }
 
+void fade(Adafruit_CPlay_NeoPixel& strip)
+{
+    for (int i = 0; i < nb_leds; ++i) {
+        auto prev_col = strip.getPixelColor(i);
+        auto falloff  = 10;
+        strip.setPixelColor(i,
+                            clamp(red(prev_col) - falloff),
+                            clamp(green(prev_col) - falloff),
+                            clamp(blue(prev_col) - falloff));
+    }
+}
+
+void ping_pong(Adafruit_CPlay_NeoPixel& strip)
+{
+    static int pos1 = 10;
+    static int dir1 = 1;
+    static int pos2 = 17;
+    static int dir2 = -1;
+    if (pos1 == 10)
+        strip.setPixelColor(0, 255, 110, 200);
+    pos1 = 1;
+    fade(strip);
+}
+
 void setup()
 {
     CircuitPlayground.begin();
@@ -40,11 +69,9 @@ void setup()
     strip.begin();
 }
 
-int time = 0;
-
 void loop()
 {
-    gradient(strip);
+    ping_pong(strip);
     strip.show();
     delay(50);
 }
